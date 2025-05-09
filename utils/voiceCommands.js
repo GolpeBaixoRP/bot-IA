274d0b76e3788c6d6de75830465f2fe30e34a6ac
GolpeBaixoRP <perigobronks@gmail.com>
2025-05-09 16:36:45 -0300

    Update voiceCommands.js
    att voice

--- utils/voiceCommands.js
+++ utils/voiceCommands.js
@@ -1,4 +1,3 @@
-
-exports.handleVoiceCommand = async (text, userId) => {
+export async function handleVoiceCommand(text, userId) {
   return { audioPath: null, response: "Comando processado: " + text };
-};
+}
