2f156ff0407c9e7a5a3801b5118e539b3cb400c2
GolpeBaixoRP <perigobronks@gmail.com>
2025-05-09 15:32:47 -0300

    Update index.js
    import 'dotenv/config';

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-require('dotenv').config();
+import 'dotenv/config';
 const fs = require('fs');
 const path = require('path');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
